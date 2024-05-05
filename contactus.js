d90ea743d97c77743058b434b9053038c912badc
youssefsaleh1 <youssefsalleh2002@gmail.com>
2024-05-06 01:45:56 +0300

    fixing

--- contactus.js
+++ contactus.js
@@ -1,80 +1,116 @@
+// const name=document.getElementById('cname')
+//  const phone=document.getElementById('cphone')
 
-.sendicon{
-    width: 35px;
-}
+// function validate(field){
+//     if(field=='')
+//         return 'You must enter all required fields';
+//     else
+//         return '';
+// }
 
-*{
-    margin: 0;
-    padding: 0;
-    font-family:'Times New Roman';
-    box-sizing: border-box;
-}
-body{
-     height: 100vh; /*vh represents the viewport-percentage length unit based on the browser default viewport size */
-    background-color: #e6f5ff;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+// function validatePassword(field1, field2){
+//     if(field1=='' || field2=='')
+//         return 'You Must Enter a Password.';
+//     else if(field1!=field2)
+//         return 'You must Enter a matching Password.';
+//     else
+//         return '';
+// }
+// function validateDOB(field){
+//     let dob = new Date(field);  
+//     let diff_ms = Date.now() - dob.getTime();
+//     let age_dt = new Date(diff_ms); 
+//     if( Math.abs(age_dt.getUTCFullYear() - 1970)>=18)
+//         return '';
+//     else
+//         return 'You Must be older than 18 years.';
+// }
+// function validate(form){
+//     let fail='';
+//     fail+=validateUserName(form.UserName.value);
+//     fail+=validatePassword(form.Password.value, form.ConfirmPassword.value);
+//     fail+=validateDOB(form.dob.value);
+//     if(fail=='')
+//         return true;
+//     else{
+//         alert(fail);
+//         return false;
+//     }
+// }
+// let namein=document.querySelector("[name='cname']");
+// let phin=document.querySelector("[name='cphone']");
+// let emailin=document.querySelector("[name='cmail']");
+// let mesin=document.querySelector("[name='cmes']");
 
-}
-.container{
-    width: 450px;
-    height: 80vh;
-    display: flex;
-    flex-direction: column;
-    box-shadow: 2px 2px 2px beige;
-background-color: beige;
-}
-.header h1{
-    color: black;
-    padding: 10px;
-    background-color: #0000FF;
-}
-.body{
-    flex:1;
-    color: black;
-    background-color: beige
-    ;
-    padding: 20px 30px;
-}
-form{
-background-color: #0000FF;
-}
-.message{
-    background-color: #0000FF;
-    padding: 10px;
-    color: white;
-    width: fit-content;
-    border-radius: 10px;
-  margin-bottom: 15px;
-}
-.user_message{
-    margin-left: auto;
-    background-color: white;
-    color: black;
-}
-.footer {
-    display: flex;
-}
- input{
-    flex:1;
-    height: 40px;
-    border:none;
-    outline: none;
-    padding-left:5px ;
-    font-size:16 px;
-    background-color: #00b7f1;
-}
- button{
-    width: 100px;
-    font-size: 18px;
-    border:none;
-    outline: none;
-    color: white;
-    cursor: pointer;
-}
- button:hoover{
-    background-color: #0000FF;
-    transition: 0.2s ease;
 
-}
+
+// document.forms[0].onsubmit=function(c){
+// let namev=false;
+// let phv=false;
+// let emailv=false;
+// let mesv=false;
+ 
+// if(namein.value !==""){
+//    namev=true;
+// }
+// if(phin.value !==""){
+//     phv=true;
+//  }
+//  if(emailin.value !==""){
+//     emailv=true;
+//  }
+//  if(mesin.value !==""){
+//     mesv=true;
+//  }
+
+
+// if(namev===false || phv===false || emailv=== false || mesv===false){
+//     c.preventDefault();
+// }
+
+//}
+function validate() {
+    let namein = document.forms.contactForm.cname.value;
+    let phin = document.forms.contactForm.cphone.value;
+    let emailin = document.forms.contactForm.cmail.value;
+    let mesin = document.forms.contactForm.cmes.value;
+    //window.alert('s'+mesin+'m');
+  
+    if (namein == "") {
+     document.getElementById("label1").style.visibility = 'visible';
+    }
+    else{
+     document.getElementById("label1").style.visibility = 'hidden';
+    }
+    if (phin == "") {
+     document.getElementById("label2").style.visibility = 'visible';
+    }
+    else{
+     document.getElementById("label2").style.visibility = 'hidden';
+    }
+    if (emailin == "") {
+     document.getElementById("label3").style.visibility = 'visible';
+    }
+    else{
+     document.getElementById("label3").style.visibility = 'hidden';
+    }
+    if (mesin == "") {
+  
+     document.getElementById("label5").style.visibility = 'visible';
+    }
+    else{
+     document.getElementById("label5").style.visibility = 'hidden';
+    }
+    if(phin.length!=11 && phin.length!=""){
+     document.getElementById("label6").style.visibility = 'visible';
+    }
+    else{
+     document.getElementById("label6").style.visibility = 'hidden';
+    }
+  
+   if(namein == ""||phin == ""||emailin == ""||mesin == ""||phin.length!=11){
+     return false;
+   }
+   
+  }
+ 
