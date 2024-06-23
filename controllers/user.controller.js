3e1c486b1aa2d2eb7b03a9a61c6f407f81351815
mohamed0tamer <mohamed2111896@miuegypt.edu.eg>
2024-06-24 02:35:37 +0300

    usercontroller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -49,3 +49,52 @@ const validation = [
     .custom((value, { req }) => value === req.body.password)
     .withMessage("Passwords do not match"),
 ];
+const signup = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("pages/register", {
+        title: "Signup page - Validation Failed",
+        errors: errors.array(),
+      });
+      return;
+    }
+    try {
+      const hashedPassword = await bcrypt.hash(req.body.password,saltRounds);
+      const existingUser = await User.findOne({ username: req.body.username });
+      const existingemail = await User.findOne({ email: req.body.email });
+  
+      if (existingemail) {
+        console.log("Email already exists");
+        res.send("Email already exists");
+      }else if(existingUser){
+        console.log("username already exists");
+        res.send("username already exists");
+      } else {
+        const newUser = new User({
+          username: req.body.username,
+          email: req.body.email,
+          password: hashedPassword,
+          type:req.body.type,
+          photo:"profile.jpg",
+        });
+  
+        await newUser.save().then(result =>{
+          req.session.user=result;
+  
+        })
+  
+  
+        console.log("User saved successfully");
+        res.redirect('/');
+      }
+    } catch (error) {
+      console.log(error);
+      res.send("An error occurred");
+    }
+  };
+  
+  // const logvalidation = [
+  //  body("logusername").notEmpty().withMessage("Username is required"),
+  //   body("logpassword").notEmpty().withMessage("password is required"),
+  
+  // ];
