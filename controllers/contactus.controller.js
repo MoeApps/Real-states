5836761df059d5adbf2800912efe792a42782d16
Joewilliam1 <youssef2103603@miuegypt.edu.eg>
2024-06-24 16:17:46 +0300

    Updated contactus.controller.js
    Created addContactUsMsg

--- controllers/contactus.controller.js
+++ controllers/contactus.controller.js
@@ -12,3 +12,30 @@ const contactUsValidation = [
   body("cmes").notEmpty().withMessage("Message is required"),
 ];
 
+const addContactUsMsg =  async (req, res, next) => {
+    console.log("b");
+    const { cname, cmail, cphone, cloc,cmes } = req.body;
+    console.log("c");
+    try {
+      console.log("a");
+      const contact = new Contactus({
+        name: cname,
+        phoneNumber: cphone,
+        email: cmail,
+        location: cloc,
+        message: cmes
+        //
+        });
+        contact.save()
+      
+        .catch(err => {
+          console.log(err);
+        });
+    }
+    catch (error) {
+      res.send("Message Could not be Sent");
+    }
+    res.send("Message Succssfully Sent!");
+  };
+  
+  export { addContactUsMsg };
