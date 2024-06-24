e23923d258459213b784a9890bb80dd8331c6ea3
abouelhadid2 <marwan2106369@miuegypt.edu.eg>
2024-06-24 04:37:47 +0300

    admin route

--- routes/admin.rout.js
+++ routes/admin.rout.js
@@ -9,3 +9,26 @@ import{
   chats
 }from "../controllers/chat.controller.js"
 const router = Router();
+
+router.use((req, res, next) => {
+  if (req.session.user !== undefined && req.session.user.type === 'admin') {
+      next();
+  }
+  else {
+      res.render('pages/err', { err: 'You are not an Admin',user: (req.session.user === undefined ? "" : req.session.user) })
+  }
+});
+
+router.get('/', getalluser);
+router.get('/delprop/:id',deleteprop);
+router.get('/editprop/:id',getprop)
+router.get('/deleteuser/:id', (req, res, next) => {
+  console.log(req.params.id)
+  User.findByIdAndDelete(req.params.id)
+    .then(result => {
+      res.redirect('/admin/viewusers');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
